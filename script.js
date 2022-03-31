c43e2ad9e48f394ff6e0e7288884326d6a6c2ba5
Tiago Sebastian <tiagosebastian99@gmail.com>
2022-03-31 18:11:34 -0300

    Temporização

--- script.js
+++ script.js
@@ -1,27 +1,10 @@
-document.write("</br> Escolha seu pedido: </br>");
-document.write("</br> 0 - Sorvete / 1 - Suco </br>");
-document.write("</br> 2 - coca cola / 3 - Ahua gelada </br></br>");
 
-//Switch
-function pedir(){
-    x = prompt("O que deseja pedir");
-
-    switch(x){
-    
-        case"0":
-            alert("Voce pediu sorvete :) ");
-            break;
-        case "1":
-            alert("Voce pediu um suco ");
-            break;
-        case "2":
-            alert("Voce pediu uma coca ");
-            break;
-        case "3":
-            alert("Voce pediu uma agua geleda ");
-            break;
-            default:
-                alert("Ops nao temos essa opcao!");
-                break;
-    }
+function acao (){
+    document.write("Executando...</br>");
 }
+
+// Executa de tempo em tempo!
+//setInterval(acao, 1000);
+
+//Executa e para 
+setTimeout(acao, 3000);
