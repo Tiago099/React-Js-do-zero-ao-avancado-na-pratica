43a06b3155e6e7376f02000a3915d623e6a70588
Tiago Sebastian <tiagosebastian99@gmail.com>
2022-04-12 18:54:38 -0300

    Trabalhando com states

--- minhapagina/src/App.js
+++ minhapagina/src/App.js
@@ -1,49 +1,47 @@
 import React, { Component } from 'react';
 
-class Equipe extends Component{
-    render(){
-        return(
-            <div>
-                <Sobre nome={this.props.nome} cargo={this.props.cargo}
-                 idade={this.props.idade}/>
-                 <Social/>
-                 <hr/>
-                
-            </div>
-        );
-    }
-}
-class Sobre extends Component{
-    render(){
-        return(
-        <div>
-              <h2>Olá sou o(a){this.props.nome}</h2>
-                <h3>Cargo:{this.props.cargo}</h3>
-                <h3> Idade:{this.props.idade}</h3> 
-        </div>
-        
-        );
-    }
-        
-}
-const Social = () =>{
-    return(
-        <div>
-             <a> Facebook </a>
-             <a>LinkedIn</a>
-        </div>
-    )
-        
-    
-}
 
-function App(){
-    return(
-        <div>
-            <h1>Conheça nossa equipe:</h1>
-            <Equipe nome="Tiago" cargo="Desenvolvedor" idade="28"/>    
-            <Equipe nome="Luan" cargo="Designer" idade="25"/>   
-        </div>
-    )
-}
+    
+class App extends Component{
+  constructor(props){
+      super(props);
+      this.state = {
+          nome: 'Tiago',
+          contador:0
+      };
+
+
+      this.aumentar = this.aumentar.bind(this);
+      this.diminuir = this.diminuir.bind(this);
+  }
+  aumentar(){
+      let state = this.state;
+      state.contador += 1;
+      state.nome = 'Lucas';
+      this.setState(state);
+  }
+  diminuir(){
+      let state = this.state;
+      if(state.contador === 0){
+          alert('Opa chegou a zero!');
+          return;
+      }
+      state.contador -= 1;
+      this.setState(state);
+  }
+  render(){
+      return(
+          <div>
+              <h1>Contador</h1>
+              {this.state.nome}
+              <h3>
+                  <button onClick={this.diminuir}>-</button>
+                       {this.state.contador}
+                  <button onClick={this.aumentar}>+</button>
+               </h3>
+          </div>
+        );
+        
+    }
+  }
 export default App;
