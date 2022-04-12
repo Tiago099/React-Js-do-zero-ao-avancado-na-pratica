bc5082ac7d89991cb04ca45321ff2658dfbb701f
Tiago Sebastian <tiagosebastian99@gmail.com>
2022-04-12 17:50:48 -0300

    Aplicando Class component

--- minhapagina/src/App.js
+++ minhapagina/src/App.js
@@ -1,47 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-
-const Equipe = (props) =>{
-    return(
-        <div>
-            <Sobre nome={props.nome} cargo={props.cargo} idade={props.idade}/>
-            <Social fb={props.Facebook}/>
-            <hr/>
-        </div>
-    );
+class Equipe extends Component{
+    render(){
+        return(
+            <div>
+                <Sobre nome={this.props.nome} cargo={this.props.cargo}
+                 idade={this.props.idade}/>
+                 <Social/>
+                 <hr/>
+                
+            </div>
+        );
+    }
 }
-
-const Sobre =(props) => {
+class Sobre extends Component{
+    render(){
+        return(
+        <div>
+              <h2>Olá sou o(a){this.props.nome}</h2>
+                <h3>Cargo:{this.props.cargo}</h3>
+                <h3> Idade:{this.props.idade}</h3> 
+        </div>
+        
+        );
+    }
+        
+}
+const Social = () =>{
     return(
         <div>
-            <h2> Olá sou o(a) {props.nome}</h2>
-            <h3>Sou {props.cargo}</h3>
-            <h4>tenho {props.idade} anos</h4>
+             <a> Facebook </a>
+             <a>LinkedIn</a>
         </div>
     )
-}
-const Social = (props) => {
-    return (
-        <div>
-        <a href={props.fb}>Facebook </a>
-        <a>Linkedin </a>
-        <a>Youtube </a>
-        </div>
-    );
+        
+    
 }
 
 function App(){
     return(
         <div>
             <h1>Conheça nossa equipe:</h1>
-            <Equipe nome="Tiago" cargo="Programador" idade="30"
-                   Facebook="https://google.com"/>
-            <Equipe nome="João" cargo="Designer" idade="35"
-                   Facebook="https://google.com"/>
-            <Equipe nome="Amanda" cargo="Front-end" idade="27"
-                   Facebook="https://google.com"/>
-           
-           
+            <Equipe nome="Tiago" cargo="Desenvolvedor" idade="28"/>    
+            <Equipe nome="Luan" cargo="Designer" idade="25"/>   
         </div>
     )
 }
