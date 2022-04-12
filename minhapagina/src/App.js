377698371ca46ad6b4cf223279e888e67a4e359e
Tiago Sebastian <tiagosebastian99@gmail.com>
2022-04-12 19:26:39 -0300

    Ciclo de vida dos componentes

--- minhapagina/src/App.js
+++ minhapagina/src/App.js
@@ -3,42 +3,32 @@ import React, { Component } from 'react';
 
     
 class App extends Component{
-  constructor(props){
-      super(props);
-      this.state = {
-          nome: 'Tiago',
-          contador:0
-      };
+
+    constructor(props){
+        super (props);
+        this.state = {
+            hora: '00:00:00'
+         };
+    }
+ // ciclo de vida componentDidMount é rodado uma unica vez
 
 
-      this.aumentar = this.aumentar.bind(this);
-      this.diminuir = this.diminuir.bind(this);
-  }
-  aumentar(){
-      let state = this.state;
-      state.contador += 1;
-      state.nome = 'Lucas';
-      this.setState(state);
-  }
-  diminuir(){
-      let state = this.state;
-      if(state.contador === 0){
-          alert('Opa chegou a zero!');
-          return;
-      }
-      state.contador -= 1;
-      this.setState(state);
-  }
+    componentDidMount(){
+        setInterval(()=>{
+            this.setState({ hora: new Date().toLocaleTimeString() })
+        }, 1000);
+    }
+
+    //ciclo de vida componetDidUpdate toda vez que precisa atualiza um state
+
+    componentDidUpdate(){
+        console.log('Atualizou!!!');
+    }
   render(){
       return(
           <div>
-              <h1>Contador</h1>
-              {this.state.nome}
-              <h3>
-                  <button onClick={this.diminuir}>-</button>
-                       {this.state.contador}
-                  <button onClick={this.aumentar}>+</button>
-               </h3>
+              <h1>Meu Projeto {this.state.hora}</h1>
+            
           </div>
         );
         
