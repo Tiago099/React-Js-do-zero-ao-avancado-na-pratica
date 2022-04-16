22ff34464711a5598904b51033d49070357bb839
Tiago Sebastian <tiagosebastian99@gmail.com>
2022-04-16 12:54:34 -0300

    Eventos

--- minhapagina/src/App.js
+++ minhapagina/src/App.js
@@ -1,34 +1,13 @@
 import React, { Component } from 'react';
-
+import Membro from './Components/Membro';
 
     
 class App extends Component{
 
-    constructor(props){
-        super (props);
-        this.state = {
-            hora: '00:00:00'
-         };
-    }
- // ciclo de vida componentDidMount é rodado uma unica vez
-
-
-    componentDidMount(){
-        setInterval(()=>{
-            this.setState({ hora: new Date().toLocaleTimeString() })
-        }, 1000);
-    }
-
-    //ciclo de vida componetDidUpdate toda vez que precisa atualiza um state
-
-    componentDidUpdate(){
-        console.log('Atualizou!!!');
-    }
   render(){
       return(
           <div>
-              <h1>Meu Projeto {this.state.hora}</h1>
-            
+              <Membro nome= "Visitante"/>
           </div>
         );
         
