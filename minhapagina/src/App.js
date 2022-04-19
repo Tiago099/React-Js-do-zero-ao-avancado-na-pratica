0ea93bee95e0dde2e537b455b7451172e3bf3ceb
Tiago Sebastian <tiagosebastian99@gmail.com>
2022-04-19 19:48:42 -0300

    Trabalhando com Lista

--- minhapagina/src/App.js
+++ minhapagina/src/App.js
@@ -1,70 +1,41 @@
 import React, { Component } from 'react';
+import Feed from './Components/Feed';
 
 
     
 class App extends Component{
-// Renderização condicional normal
-  /*constructor(props){
-    super(props);
-    this.state = {
-      status: 2
 
-    };
-  }
-
-  render(){
-      return(
-          <div>
-            {this.state.status === 1 &&
-                <h1>Bem-vindo ao sistema!</h1>
-            }
-
-        <div>
-          <h2>Curso de React Js</h2>
-        </div>
-
-      </div>
-        );
-        
-    }
-  }
-  */
- // Renderização com true e false
   constructor(props){
     super(props);
     this.state = {
-      status: false
-
+      feed:[
+        {id: 1, username: 'Joao', curtidas:15, comentarios:3},
+        {id: 2, username: 'Mateus', curtidas:100, comentarios:40},
+        {id: 3, username: 'Tiago', curtidas:250, comentarios:100},
+        {id: 4, username: 'Maria'  , curtidas:50, comentarios:9},
+        {id: 4, username: 'Ireu'  , curtidas:1, comentarios:1},
+      ]
+      
+    
     };
 
-    this.sair = this.sair.bind(this);
-    this.entrar = this.entrar.bind(this);
-  }
-  sair(){
-    this.setState({status: false});
-  }
-
-  entrar(){
-    this.setState({status: true});
   }
 
   render(){
       return(
           <div>
-            {this.state.status ?
-            <div>
-                <h2>Bem-vindo ao sistema!</h2>
-                <button onClick={this.sair}>Sair</button>
-            </div> :
-            <div>
-                <h2>Olá visitante, faça o login!</h2>
-                <button onClick={this.entrar}>Entrar no sistema</button>
-            
-            </div>    
-          }
 
-        </div>
-
+            {this.state.feed.map((item)=>{
+              return(
+                
+                  <Feed key={item.id} username={item.username}
+                  curtidas={item.curtidas} comentarios={item.comentarios}/>
+          
+              );
+            })}
+          
+          </div>    
+          
       );
          
   }
