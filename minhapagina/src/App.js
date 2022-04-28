c4b597827835cf850cf3e056bf3459bcafd1665d
Tiago Sebastian <tiagosebastian99@gmail.com>
2022-04-28 17:18:59 -0300

    Aprendendo HTTP

--- minhapagina/src/App.js
+++ minhapagina/src/App.js
@@ -1,45 +1,44 @@
-import React, { useState, useEffect, useMemo, useCallback} from 'react';
+import React, {useEffect, useState} from 'react';
+import './style.css';
 
-function App() {
+///https://sujeitoprogramador.com/rn-api/?api=posts 
+function App(){
+    const[nutri, setNutri] = useState([]);
 
-    const [tarefas, setTarefas] = useState([]);
-const [input, setInput] = useState('');
+    useEffect(() => {
 
-useEffect(()=>{
-    const tarefasStorage = localStorage.getItem('tarefas');
+        function loadApi(){
+           let url = 'https://sujeitoprogramador.com/rn-api/?api=posts ';
 
-    if(tarefasStorage){
-        setTarefas(JSON.parse(tarefasStorage));
-    }
-},[]);
+           fetch(url)
+           .then((r) => r.json())
+           .then((json) => {
+               setNutri(json);
+           })
+        }
 
-useEffect(()=> {
-    localStorage.setItem('tarefas', JSON.stringify(tarefas));
-}, [tarefas])
+        loadApi();
+    }, []);
 
+    return (
+        <div className="container">
+            <header>
+                <strong>React Nutri</strong>
+            </header>
+            {nutri.map((item) =>{
+                return(
+                    <article key={item.id} className="post">
+                        <strong className="titulo">{item.titulo}</strong>
 
-const handleAdd = useCallback(() =>{
-    setTarefas([...tarefas, input]);
-    setInput('');
-}, [input, tarefas]);
-
-const totalTarefas = useMemo(() => tarefas.length, [tarefas])
-
-
-  return(
-    <div>
-        <ul>
-            {tarefas.map(tarefa =>(
-                <li key={tarefa}>{tarefa}</li>
-            ))}
-        </ul>
-        <br/>
-         <strong>Voce tem {totalTarefas} taredas!</strong><br/>
-        <input type="text" value={input} onChange={ e => setInput(e.target.value)}/>
-        <button type= "button" onClick ={handleAdd}>Adicionar</button>
-            
-    </div>
-  );
-    
+                        <img src={item.capa} alt={item.titulo} className="capa"/>
+                        <p className="subtitulo">
+                            {item.subtitulo}
+                        </p>
+                        <a className="botao">Acessar</a>
+                    </article>
+                )
+            })}
+       </div>
+    );
 }
 export default App;
