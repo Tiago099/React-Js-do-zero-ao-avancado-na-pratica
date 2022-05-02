ba288db0fba795c1eb274a93e36ec11068c28514
Tiago Sebastian <tiagosebastian99@gmail.com>
2022-05-02 19:07:37 -0300

    Melhorando nossas Rotas

--- minhapagina/src/routes.js
+++ minhapagina/src/routes.js
@@ -1,13 +1,16 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import Contato from './pages/Contato';
 
+import Contato from './pages/Contato';
 import Home from './pages/Home';
 import Sobre from'./pages/Sobre';
+
+import Header from './components/Header';
 
 
 function RoutesApp(){
     return(
         <BrowserRouter>
+        <Header/>
            <Routes>
             <Route path="/"element={ <Home/> }/>
             <Route path="/sobre" element={ <Sobre/> } />
