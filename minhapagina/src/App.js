358b5ce6fad6b14b9758ea1f4503192cb8142a27
Tiago Sebastian <tiagosebastian99@gmail.com>
2022-05-02 19:46:54 -0300

    Melhorando nossas Rotas pt2

--- minhapagina/src/App.js
+++ minhapagina/src/App.js
@@ -2,10 +2,7 @@ import RoutesApp from './routes';
 
 function App() {
   return (
-    
     <RoutesApp/>
- 
-    
   );
 }
 
---
+++ minhapagina/src/pages/Erro/index.js
@@ -0,0 +1,9 @@
+
+
+function Erro() {
+    return (
+     <h1> Ops parece que esta pagina não existe</h1>
+    );
+  }
+  
+  export default Erro;
