d2eca7ee7a8be8b905fed776a92bcc51265a610f
Tiago Sebastian <tiagosebastian99@gmail.com>
2022-05-14 01:41:38 -0300

    Carregando filmes

--- minhapagina/src/pages/Home/index.js
+++ minhapagina/src/pages/Home/index.js
@@ -6,7 +6,7 @@ import './home.css';
 
 function Home(){
   const [filmes, setFilmes] = useState([]);
-
+  const [loading,setLoading] = useState(true);
   useEffect(()=>{
 
     async function loadFilmes(){
@@ -20,17 +20,26 @@ function Home(){
      })
      //console.log(response.data.results.slice(0, 10));
      setFilmes(response.data.results.slice(0, 10));
+     setLoading(false);
     }
 
     loadFilmes();
 
   }, [])
+  
+  if(loading){
+      return(
+          <div className='loading'>
+              <h2>Carregando filmes...</h2>
+          </div>
+      )
+  }
     return(
         <div className='container'>
             <div className='lista-filmes'>
             {filmes.map((filme) =>{
                 return(
-                    <article>
+                    <article key={filme.id}>
                         <strong>{filme.title}</strong>
                         <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title}/>
                         <Link to={`/filme${filme.id}`}>Acessar</Link>
