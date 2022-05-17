66de96b02214fcf3e12596da5549ab07034f381e
Tiago Sebastian <tiagosebastian99@gmail.com>
2022-05-17 18:28:02 -0300

    Detalhes do filme

--- minhapagina/src/pages/Filme/index.js
+++ minhapagina/src/pages/Filme/index.js
@@ -1,10 +1,59 @@
+import {useEffect, useState} from 'react';
+import {useParams} from 'react-router-dom';
+
+import api from '../../services/api';
+
 
 
 function Filme(){
+    const{ id } = useParams();
+    const[filme, setFilmes]=useState({});
+    const[loading, setLoading]=useState(true);
+
+    useEffect(()=>{
+        async function loadFilmes(){
+            await api.get (`/movie/${id}`,{
+                params:{
+                    api_key: "183e52d87b0d6e6b05b38d3d9b6946c3",
+             language: "pt-BR",
+                }
+            })
+            .then((response)=>{
+                console.log(response);
+                setFilmes(response.data);
+                setLoading(false);
+            })
+            .catch(()=>{
+                console.log("FILME NAO ENCONTARDADO");
+            })
+
+        }
+
+        loadFilmes();
+
+        return ()=>{
+
+        }
+
+    }, [id])
+
+    if(loading){
+        return(
+            <div className='filme-info'>
+                 <h1>Carregando detalhes.....</h1>
+            </div>
+        )
+    }
     return(
-        <di>
-            <h1>Bem vindo ao detalhe do Filme</h1>
-        </di>
+        <div className='filme-info'>
+            <h1>{filme.title}</h1>
+            <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title} />
+
+            <h3>Sinopse</h3>
+            <span>{filme.overview}</span>
+
+            <strong>Avaliação: {filme.vote_average} /10 </strong>
+        </div>
     );
 }
 
