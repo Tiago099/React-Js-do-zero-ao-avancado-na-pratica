a874ceb6738409195a8ea2445c22e579650c0723
Tiago Sebastian <tiagosebastian99@gmail.com>
2022-05-19 18:55:58 -0300

    Personalizando alertas do sistema

--- minhapagina/src/pages/Filme/index.js
+++ minhapagina/src/pages/Filme/index.js
@@ -2,6 +2,7 @@ import {useEffect, useState} from 'react';
 import {useParams, useNavigate} from 'react-router-dom';
 import './filme-info.css';
 import api from '../../services/api';
+import { toast } from 'react-toastify';
 
 
 
@@ -51,13 +52,13 @@ function Filme(){
         const hasFilme = filmesSalvos.some((filmeSalvo)=> filmeSalvo.id === filme.id)
 
         if(hasFilme){
-            alert("ESSE FILME JÁ ESTA NA LISTA");
+            toast.warn("ESSE FILME JÁ ESTA NA LISTA");
             return;
         }
 
         filmesSalvos.push(filme);
         localStorage.setItem("@primeflix", JSON.stringify(filmesSalvos));
-        alert("FILME SALVO COM SUCESSO")
+        toast.success("Filme salvo com sucesso")
     }
 
 
