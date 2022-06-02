7c173646a7a8275ca975ac29505f3af820abdcbd
Tiago Sebastian <tiagosebastian99@gmail.com>
2022-06-02 20:11:18 -0300

    Criando alertas

--- sistema/src/contexts/auth.js
+++ sistema/src/contexts/auth.js
@@ -1,5 +1,6 @@
 import { useState, createContext, useEffect } from "react";
 import firebase from "../services/firebaseConnection";
+import { toast, ToastContainer } from "react-toastify";
 
 
 export const AuthContext = createContext({});
@@ -48,11 +49,15 @@ function AuthProvider({ children}){
         setUser(data);
         storageUser(data);
         setLoadingAuth(false);
+        toast.success('Bem vindo de volta!');
+        
       })
 
       .catch((error)=>{
         console.log(error);
+        toast.error('Ops algo deu errado! ');
         setLoadingAuth(false);
+        
 
       })
     }
@@ -81,12 +86,14 @@ function AuthProvider({ children}){
         setUser(data);
         storageUser(data);
         setLoadingAuth(false);
+        toast.success('Bem vindo a plataforma!');
 
       })
 
     })
     .catch((error)=>{
       console.log(error);
+      toast.error('Ops algo deu errado!');
       setLoadingAuth(false);
     })
   }
