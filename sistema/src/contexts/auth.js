6acecb127ca3e5c9cb98cbe4e62db74502abd32e
Tiago Sebastian <tiagosebastian99@gmail.com>
2022-06-02 19:43:43 -0300

    Logando usuario

--- sistema/src/contexts/auth.js
+++ sistema/src/contexts/auth.js
@@ -27,8 +27,37 @@ function AuthProvider({ children}){
     loadStorage();
 
     },[])
-    
 
+// Fazendo login do usuario
+    async function signIn(email, password){
+      setLoadingAuth(true);
+
+      await firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(async (value)=> {
+        let uid = value.user.uid;
+
+        const userProfile = await firebase.firestore().collection('users')
+        .doc(uid).get();
+
+        let data = {
+          uid: uid,
+          nome: userProfile.data().nome,
+          avatarUrl: userProfile.data().avatarUrl,
+          email: value.user.email
+        };
+        setUser(data);
+        storageUser(data);
+        setLoadingAuth(false);
+      })
+
+      .catch((error)=>{
+        console.log(error);
+        setLoadingAuth(false);
+
+      })
+    }
+    
+//Cadastrando um novo usuario
     async function signUp(email, password, nome){
       setLoadingAuth(true);
       await firebase.auth().createUserWithEmailAndPassword(email,password)
@@ -66,6 +95,7 @@ function AuthProvider({ children}){
     localStorage.setItem('SistemaUser', JSON.stringify(data));
   }
 
+//logout do usuario
   async function signOut(){
     await firebase.auth().signOut();
     localStorage.removeItem('SistemaUser');
@@ -75,7 +105,17 @@ function AuthProvider({ children}){
 
       
     return(
-        <AuthContext.Provider value={{signed: !!user, user, loading, signUp, signOut }}>
+        <AuthContext.Provider 
+        value={{
+          signed: !!user,
+          user,
+          loading,
+          signUp,
+          signOut, 
+          signIn,
+          loadingAuth
+        }}
+        >
           {children}
         </AuthContext.Provider>
     )
--- sistema/src/pages/SignUp/index.js
+++ sistema/src/pages/SignUp/index.js
@@ -11,7 +11,7 @@ const [nome, setNome] = useState('');
 const [email, setEmail] = useState('');
 const [password, setPassword] = useState('');
 
-const { signUp } = useContext(AuthContext);
+const { signUp,loadingAuth } = useContext(AuthContext);
 
 
 function handleSubmit(e){
@@ -36,7 +36,7 @@ function handleSubmit(e){
               <input type="text" placeholder="Seu nome" value={nome} onChange={ (e) => setNome(e.target.value)} />
               <input type="text" placeholder="email@email.com" value={email} onChange={ (e) => setEmail(e.target.value) }/>
               <input type="password" placeholder="*********" value={password} onChange={ (e) => setPassword(e.target.value) }/>
-              <button type="submit">Cadastrar</button>
+              <button type="submit">{loadingAuth ? 'Carregando...' : 'Cadastrar'}</button>
             </form>
 
             <Link to="/">Já tem uma conta? Entre</Link>
