4894a284181fa81c4ef095e295ab5baea7d63b24
Tiago Sebastian <tiagosebastian99@gmail.com>
2022-06-06 19:28:57 -0300

    Editanto perfil

--- sistema/src/contexts/auth.js
+++ sistema/src/contexts/auth.js
@@ -120,7 +120,9 @@ function AuthProvider({ children}){
           signUp,
           signOut, 
           signIn,
-          loadingAuth
+          loadingAuth,
+          setUser,
+          storageUser
         }}
         >
           {children}
