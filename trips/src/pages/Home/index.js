479f0298e0bf6bb5821a03feb0758fef5c1c1a34
Tiago Sebastian <tiagosebastian99@gmail.com>
2022-06-20 13:20:20 -0300

    Estilizando Home

--- trips/src/pages/Home/index.js
+++ trips/src/pages/Home/index.js
@@ -1,5 +1,9 @@
 import React, { useState, useEffect } from 'react';
+import {MdFlightTakeoff} from 'react-icons/md';
+
+
 import api from '../../services/api';
+import './style.css';
 
 export default function Home() {
   const [trips, setTrips] = useState ([]);
@@ -10,7 +14,7 @@ export default function Home() {
       const response = await api.get('trips');
       setTrips(response.data);
 
-      console.log(response.data);
+   
     }
 
     loadApi();
@@ -22,7 +26,26 @@ export default function Home() {
 
  return (
    <div>
-    <h1>Home!</h1>
+    <div className='box'>
+      {trips.map(trip => (
+        <li key={trip.id}>
+          <img src={trip.image} alt={trip.title}/>
+          <strong>{trip.title}</strong>
+          <span>Status: {trip.status ? 'Disponivel' : 'Indisponivel'}</span>
+
+          <button
+          type='button'
+          onClick={()=>{}}
+          >
+            <div>
+              <MdFlightTakeoff size={16} color="$FFF" />
+            </div>
+            <span>SOLICITAR RESERVA</span>
+          </button>
+        </li>
+      ))}
+
+    </div>
    </div>
  );
 }
