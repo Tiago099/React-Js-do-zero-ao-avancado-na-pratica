c723bb6667850d98eb10e90d5d0370d2fead83d8
Tiago Sebastian <tiagosebastian99@gmail.com>
2022-06-22 16:56:38 -0300

    Configurando Redux Saga

--- trips/src/store/modules/reserve/actions.js
+++ trips/src/store/modules/reserve/actions.js
@@ -1,8 +1,15 @@
-export function addReserve(trip){
+export function addReserveRequest(id){
     return{
-      type: 'ADD_RESERVE',
-      trip    
+      type: 'ADD_RESERVE_REQUEST',
+      id    
     }
+}
+
+export function addReserveSuccess(trip){
+  return{
+    type: 'ADD_RESERVE_SUCCESS',
+    trip   
+  }
 }
 
 export function removeReserve(id){
