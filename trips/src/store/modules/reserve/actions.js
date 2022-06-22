5c374fe1db5237a29fab1e00d7240bb8c4aca329
Tiago Sebastian <tiagosebastian99@gmail.com>
2022-06-22 02:25:08 -0300

    Mudando a quantidade

--- trips/src/store/modules/reserve/actions.js
+++ trips/src/store/modules/reserve/actions.js
@@ -1,16 +1,21 @@
 export function addReserve(trip){
     return{
       type: 'ADD_RESERVE',
-      trip
-         
+      trip    
     }
 }
 
 export function removeReserve(id){
     return{
       type: 'REMOVE_RESERVE',
-      id,
-      
-          
+      id,   
     }
+}
+
+export function updateAmountReserve(id, amount){
+  return{
+    type:'UPDATE_RESERVE',
+    id,
+    amount
+  }
 }
